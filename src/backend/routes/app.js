69b1c976454470544032d7cb380931c021bb0981
devmlb <97614168+devmlb@users.noreply.github.com>
2024-11-15 19:40:40 +0100

    Seconde tentative d'implémentation d'un système de version

--- src/backend/routes/app.js
+++ src/backend/routes/app.js
@@ -1,11 +1,24 @@
 import express from "express";
+import { Octokit } from "@octokit/rest";
 import "dotenv/config";
 const router = express.Router();
 
 router.get("/version", async (req, res) => {
     try {
-        const resultatFormate = { version: process.env.VERSION };
+        // COMMENTE EN ATTENDANT DE POUVOIR UTILISER LES IDENTIFIANTS GITHUB
+        // const octokit = new Octokit({
+        //     auth: process.env.GITHUB_AUTH
+        // })
 
+        // const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+        //     owner: 'Maxence-r',
+        //     repo: 'salle-univ-nantes',
+        //     headers: {
+        //         'X-GitHub-Api-Version': '2022-11-28'
+        //     }
+        // });
+
+        const resultatFormate = { version: process.env.VERSION };
         res.json(resultatFormate);
     } catch (erreur) {
         res.status(500).send("ERREUR_INTERNE");

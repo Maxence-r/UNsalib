d729123efd83cde8236565b8acc9516ddf023ab3
Maxence <maxence.rabrais@gmail.com>
2024-11-19 00:03:00 +0100

    PWA

--- src/client/javascript/main.js
+++ src/client/javascript/main.js
@@ -38,3 +38,22 @@ socket.on("error", (error) => {
 });
 
 
+let defferedPrompt;
+const addbtn = document.querySelector('.version');
+
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault();
+    defferedPrompt = event
+    addbtn.style.display = 'block';
+});
+
+addbtn.addEventListener('click', event => {
+    defferedPrompt.prompt();
+
+    defferedPrompt.userChoice.then(choice => {
+        if(choice.outcome === 'accepted'){
+            console.log('user accepted the prompt')
+        }
+        defferedPrompt = null;
+    })
+})

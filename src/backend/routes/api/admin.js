a8ac7d459766d54984c60069c03711b1312005da
devmlb <97614168+devmlb@users.noreply.github.com>
2024-12-08 19:08:08 +0100

    Implémentation du tableau de bord
    - Création des endpoints admin dans le backend
    - Création de l'interface (sommaire)
    - Modification du schéma de salle pour prendre en charge les nouveaux champs de la base de données
    - Modification en conséquence du site principal

--- src/backend/routes/api/admin.js
+++ src/backend/routes/api/admin.js
@@ -1,12 +1,14 @@
 import express from "express";
 import Salle from "../../models/salle.js";
 import Account from '../../models/account.js';
+import mongoose from "mongoose";
 import { compare } from 'bcrypt';
 import pkg from 'jsonwebtoken';
 const router = express.Router();
 const { sign } = pkg;
 
 router.get("/update-alias", async (req, res) => {
+    if (!req.connected) return res.redirect('/admin/auth');
     try {
         const salles = await Salle.find({
             alias: { $regex: /(lle)/i }
@@ -73,6 +75,117 @@ router.post('/auth/login', async (req, res) => {
 router.get('/auth/logout', async (req, res) => {
     res.clearCookie('token');
     res.redirect('/admin/auth');
+});
+
+router.get("/rooms", async (req, res) => {
+    if (!req.connected) return res.redirect('/admin/auth');
+    try {
+        // Obtention de toutes les salles
+        let salles = await Salle.find({}).select(
+            "-__v -identifiant"
+        );
+
+        // Formatage de la réponse
+        const resultatFormate = salles.map((doc) => ({
+            id: doc._id,
+            name: doc.nom_salle
+        }));
+
+        res.json(resultatFormate);
+    } catch (erreur) {
+        res.status(500).json({
+            error: 'INTERNAL_ERROR',
+        });
+        console.error(
+            "Erreur pendant le traitement de la requête à",
+            req.url,
+            `(${erreur.message})`
+        );
+    }
+});
+
+router.get("/room", async (req, res) => {
+    if (!req.connected) return res.redirect('/admin/auth');
+    // Récupération des paramètres de la requête
+    const id = req.query.id;
+
+    // Vérification de la présence de tous les paramètres nécessaires
+    if (!id) {
+        return res.status(400).json({
+            error: 'MISSING_QUERIES',
+        });
+    }
+    // Validation de l'ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            error: 'INVALID_ID',
+        });
+    }
+
+    try {
+        let salle = await Salle.findOne({ _id: id }).select(
+            "-__v -identifiant"
+        );
+
+        // Formatage de la réponse
+        salle = {
+            id: salle._id,
+            name: salle.nom_salle,
+            alias: salle.alias,
+            seats: salle.places_assises,
+            building: salle.batiment,
+            board: salle.tableau,
+            type: salle.type,
+            details: salle.caracteristiques,
+            banned: salle.banned,
+            type: salle.type
+        };
+
+        res.status(200).json(salle);
+    } catch (erreur) {
+        res.status(500).json({
+            error: 'INTERNAL_ERROR',
+        });
+        console.error(
+            "Erreur pendant le traitement de la requête à",
+            req.url,
+            `(${erreur.message})`
+        );
+    }
+});
+
+router.post("/update-room", async (req, res) => {
+    const roomId = req.body.roomId;
+    const data = req.body.data;
+
+    if (!roomId || !data) {
+        return res.status(400).json({
+            error: 'MISSING_QUERIES',
+        });
+    }
+
+    try {
+        const succes = await Salle.findOneAndUpdate({ _id: roomId }, { $set: data }, {
+            new: true
+        });
+        if (!succes) {
+            return res.status(400).json({
+                error: 'UNKNOWN_ROOM',
+            });
+        }
+        res.status(200).json({
+            updated: true
+        });
+    } catch (erreur) {
+        res.status(500).json({
+            error: 'INTERNAL_ERROR',
+        });
+        console.error(
+            "Erreur pendant le traitement de la requête à",
+            req.url,
+            `(${erreur.message})`
+        );
+    }
 });
 
 export default router;
---
+++ src/client/javascript/admin.js
@@ -0,0 +1,130 @@
+async function updateRoom(id, data) {
+    let success = "";
+    try {
+        success = await fetch('/api/admin/update-room', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                roomId: id,
+                data: data,
+            })
+        });
+        success = await success.json();
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+    console.log(success)
+}
+
+async function getRoom(id) {
+    let data = [];
+    try {
+        data = await fetch('/api/admin/room?id=' + id, {
+            method: 'GET',
+        });
+        data = await data.json();
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+    idSection.textContent = data.id;
+    nameSection.textContent = data.name;
+    aliasSection.value = data.alias;
+    seatsSection.value = data.seats;
+    bannedSection.checked = data.banned;
+    Object.keys(data.board).forEach((type) => {
+        if (type == 'BLANC') {
+            boardsSection.querySelector('input[name="blanc"]').value = data.board[type];
+        } else if (type == 'NOIR') {
+            boardsSection.querySelector('input[name="noir"]').value = data.board[type];
+        } if (type == 'ECRAN') {
+            boardsSection.querySelector('input[name="ecran"]').value = data.board[type];
+        }
+    });
+
+    value = JSON.stringify(data.board);
+
+    detailsSection.value = JSON.stringify(data.details);
+    typeSection.value = data.type;
+}
+
+async function getRooms() {
+    const roomsList = document.querySelector('#rooms-list__container');
+    let data = [];
+    try {
+        data = await fetch('/api/admin/rooms', {
+            method: 'GET',
+        });
+        data = await data.json();
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+    data.forEach((room) => {
+        let roomElement = document.createElement('div');
+        roomElement.innerText = room.name;
+        roomElement.id = room.id;
+        roomElement.classList = 'room-item';
+        roomsList.appendChild(roomElement);
+
+        roomElement.addEventListener('click', () => {
+            getRoom(roomElement.id);
+        });
+    });
+    getRoom(document.querySelector('.room-item').id);
+    document.querySelector('#room-editor').style.display = 'flex';
+}
+
+getRooms();
+
+const idSection = document.querySelector('#id>span');
+const nameSection = document.querySelector('#name>span');
+const aliasSection = document.querySelector('#alias>input');
+const seatsSection = document.querySelector('#seats>input');
+const bannedSection = document.querySelector('#banned>input');
+const boardsSection = document.querySelector('#boards');
+const detailsSection = document.querySelector('#details>input');
+const typeSection = document.querySelector('#type>select');
+
+const saveBtn = document.querySelector('#save-button');
+saveBtn.addEventListener('click', () => {
+    const tableauObj = {
+        NOIR: boardsSection.querySelector('input[name="noir"]').value,
+        BLANC: boardsSection.querySelector('input[name="blanc"]').value,
+        ECRAN: boardsSection.querySelector('input[name="ecran"]').value
+    };
+    const data = {
+        alias: aliasSection.value,
+        places_assises: seatsSection.value,
+        tableau: tableauObj,
+        details: JSON.parse(detailsSection.value),
+        banned: bannedSection.checked,
+        type: typeSection.value
+    }
+    const id = idSection.textContent;
+    updateRoom(id, data);
+});
+
+document.querySelector("#search").addEventListener("input", (e) => {
+    let search = e.target.value
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f\s]/g, "");
+    let results = document.querySelectorAll(`.room-item`);
+    let resultsNumber = 0;
+    results.forEach((result) => {
+        let roomName = result.textContent.toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f\s]/g, "");
+        if (!roomName.includes(search)) {
+            result.style.display = "none";
+        } else {
+            resultsNumber++;
+            result.style.display = "block";
+        }
+    });
+    // document.querySelector(`.${resultContainer} .no-results`).style.display = resultsNumber > 0 ? "none" : "block";
+});

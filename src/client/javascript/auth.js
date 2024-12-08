e9090f99d8bc0646a4f5d696151772e7135896f4
devmlb <97614168+devmlb@users.noreply.github.com>
2024-12-08 13:56:15 +0100

    Amélioration de l'apparence de l'écran de connexion

--- src/client/javascript/auth.js
+++ src/client/javascript/auth.js
@@ -1,3 +1,9 @@
+function showError(error) {
+    const errorDisplay = document.querySelector('#error-display');
+    errorDisplay.textContent = error;
+    errorDisplay.style.display = 'block';
+}
+
 async function login() {
     fetch('/admin/auth', {
         method: 'POST',
@@ -5,22 +11,37 @@ async function login() {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            mail: document.getElementById('mail').value,
+            username: document.getElementById('username').value,
             password: document.getElementById('password').value,
         }),
     })
         .then(res => res.json())
         .then(data => {
             if (data.error) {
-                console.log("erreur");
+                if (data.error == 'BAD_CREDENTIALS') {
+                    showError('Nom d\'utilisateur ou mot de passe incorrect');
+                } else if (data.error == 'INTERNAL_ERROR') {
+                    showError('Erreur interne du serveur');
+                } else {
+                    showError('Erreur inconnue');
+                }
             } else {
-                console.log("succès");
-                setTimeout(() => {
-                    window.location.href = '/admin/dashboard';
-                }, 1500);
+                window.location.href = '/admin/dashboard';
             }
         })
-        .catch(err => {
-            console.log("erreur");
+        .catch((err) => {
+            console.error(err);
+            showError('Erreur inconnue');
         });
 }
+
+
+var input = document.querySelectorAll('input');
+input.forEach((input) => {
+    input.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            document.querySelector('#submit-button').click();
+        }
+    });
+});

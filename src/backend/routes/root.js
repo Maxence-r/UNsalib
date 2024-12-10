3428e023ac7e7e70e11720697515c6456353e2ce
Maxence <maxence.rabrais@gmail.com>
2024-12-10 23:43:54 +0100

    Rollback

--- src/backend/routes/root.js
+++ src/backend/routes/root.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 const router = Router();
+import "dotenv/config";
 
 router.get("/", (req, res) => {
-    console.log(process.env.MAINTENANCE);
     if (process.env.MAINTENANCE === "true") {
         res.sendFile("src/client/html/maintenance.html", { root: "." });
         return;

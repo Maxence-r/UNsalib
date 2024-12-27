f040b5e6e9a2431cb6e14e1c7cd4053d4c60e394
devmlb <97614168+devmlb@users.noreply.github.com>
2024-12-27 11:34:54 +0100

    Switched stats.js to English

--- src/backend/middlewares/stats.js
+++ src/backend/middlewares/stats.js
@@ -3,9 +3,9 @@ import crypto from 'crypto';
 const stats = async (req, res, next) => {
     const UUID = req.cookies.uuid;
 
-    if (!UUID || UUID === 'undefined') {
+    if (!UUID) {
         try {
-            const newUUID = crypto.randomBytes(16).toString("hex");
+            const newUUID = crypto.randomBytes(16).toString('hex');
             res.cookie('uuid', newUUID, { maxAge: 365 * 24 * 60 * 60 * 1000, sameSite: 'Lax' });
             req.statsUUID = newUUID;
         } finally {

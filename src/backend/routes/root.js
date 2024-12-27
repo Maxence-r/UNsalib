9baae9692b7995468743430164cc40084483e394
devmlb <97614168+devmlb@users.noreply.github.com>
2024-12-27 11:36:07 +0100

    Switched root.js to English

--- src/backend/routes/root.js
+++ src/backend/routes/root.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 const router = Router();
-import "dotenv/config";
+import 'dotenv/config';
 
-router.get("/", (req, res) => {
-    if (process.env.MAINTENANCE === "true") {
-        res.sendFile("src/client/html/maintenance.html", { root: "." });
+router.get('/', (req, res) => {
+    if (process.env.MAINTENANCE === 'true') {
+        res.sendFile('src/client/html/maintenance.html', { root: '.' });
         return;
     }
-    res.sendFile("src/client/html/main.html", { root: "." });
+    res.sendFile('src/client/html/main.html', { root: '.' });
 });
 
 
 router.get('/campus', (req, res) => {
-    res.sendFile("src/client/html/campus.html", { root: "." });
+    res.sendFile('src/client/html/campus.html', { root: '.' });
 });
 
 export default router;

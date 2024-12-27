44169e4c1ef790983cef7e0c02bdcce14a024734
devmlb <97614168+devmlb@users.noreply.github.com>
2024-12-27 11:28:57 +0100

    Switched getGroups.js to english

--- src/backend/background/getGroups.js
+++ src/backend/background/getGroups.js
@@ -1,50 +1,49 @@
-/*
-Obtient les groupes depuis le site d'emplois du temps de l'Université 
-*/
-import  Groupe from "../models/groupe.js";
-import { parse } from "node-html-parser";
-import { getCourses } from "./getCourses.js";
+import  Groupe from '../models/groupe.js';
+import { parse } from 'node-html-parser';
+import { getCourses } from './getCourses.js';
 import 'dotenv/config'
+
+const TIMETABLES_URL = 'https://edt-v2.univ-nantes.fr/sciences/educational_groups';
 
 async function getHTML(url) {
     try {
         const response = await fetch(url);
-        const html = await response.text(); // conversion de la réponse en texte brut
+        const html = await response.text(); // converting the response into plain text
         return html;
     } catch (error) {
-        console.log("Erreur lors de l'obtention de la page ", url, ":", error)
+        console.error('Erreur lors de l\'obtention de la page ', url, ':', error)
         return -1;
     }
 }
 
 async function getGroups() {
-    if (process.env.FORCER_RECUP_GPES === "false") {
-        console.log("Récupération des groupes DÉSACTIVÉE");
+    if (process.env.FORCER_RECUP_GPES === 'false') {
+        console.log('Récupération des groupes DÉSACTIVÉE');
         return getCourses();
     }
-    console.log("Récupération des groupes ACTIVÉE - Démarrage du processus...");
+    console.log('Récupération des groupes ACTIVÉE - Démarrage du processus...');
     await Groupe.deleteMany({});
-    const page = await getHTML("https://edt-v2.univ-nantes.fr/sciences/educational_groups");
-    if (page !== -1) { // il n'y a pas d'erreur dans la requête
+    const page = await getHTML(TIMETABLES_URL);
+    if (page !== -1) {
         const docRoot = parse(page);
-        const groupsInputs = docRoot.querySelectorAll("#desktopGroupForm #educational_groups input");
+        const groupsInputs = docRoot.querySelectorAll('#desktopGroupForm #educational_groups input');
         for (const input of groupsInputs) {
-            // obtient l'id de chaque case à cocher qui contient celui de l'emploi du temps
-            const group = input.id.replace("desktop-timetable-", "");
+            // Gets the id of each checkbox that contains the timetable id
+            const group = input.id.replace('desktop-timetable-', '');
             const exists = await Groupe.exists({ identifiant: group });
             if (!exists) {
-                const groupeObj = new Groupe({
+                const groupObj = new Groupe({
                     identifiant: group,
                     nom: input.nextElementSibling.textContent.trim()
                 });
-                await groupeObj.save();
-                process.stdout.write(groupsInputs.indexOf(input) + 1 + "/" + groupsInputs.length + " obtenus" + "\r");
+                await groupObj.save();
+                process.stdout.write(groupsInputs.indexOf(input) + 1 + '/' + groupsInputs.length + ' obtenus' + '\r');
             }
         }
             
         getCourses();
-    } else {
-        console.error("Erreur lors de la récupération des groupes.");
+    } else { // there is an error when fetching groups
+        console.error('Erreur lors de la récupération des groupes.');
     }
 }
 

e4e7914d9a14a7398843595f7a0e549c05b33ed5
devmlb <97614168+devmlb@users.noreply.github.com>
2024-12-29 18:03:54 +0100

    Switched getCourses.js to English

--- src/backend/background/getCourses.js
+++ src/backend/background/getCourses.js
@@ -1,171 +1,175 @@
-import Groupe from "../models/groupe.js";
-import Cours from "../models/cours.js";
-import Salle from "../models/salle.js";
-import "dotenv/config";
-import { closestPaletteColor } from "../utils/color.js";
-import io from "../../../server.js";
+import Groupe from '../models/groupe.js';
+import Cours from '../models/cours.js';
+import Salle from '../models/salle.js';
+import 'dotenv/config';
+import { closestPaletteColor } from '../utils/color.js';
+import io from '../../../server.js';
 
-// Constantes pour la configuration
-const INTERVALLE_CYCLE = 4 * 60 * 60 * 1000; // 4 heures en millisecondes
+// CONSTANTS
+// Groups update interval in milliseconds
+const CYCLE_INTERVAL = 4 * 60 * 60 * 1000; // 4 hours
+// Number of days to fetch for each timetable
+const DAYS_TO_RETRIEVE = 90; // 3 weeks
 
-// Fonction pour obtenir les dates de début et fin (3 semaines)
-const obtenirDatesRequete = () => {
-    const dateDebut = new Date();
-    const dateFin = new Date();
-    dateFin.setDate(dateFin.getDate() + 90);
-    dateDebut.setDate(dateDebut.getDate() - 1);
+// Gets the start and end dates for the request to the timetable website
+function getRequestDates(increment) {
+    const startDate = new Date();
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() + increment);
+    startDate.setDate(startDate.getDate() - 1);
 
     return {
-        debut: dateDebut.toISOString().split("T")[0],
-        fin: dateFin.toISOString().split("T")[0],
+        start: startDate.toISOString().split('T')[0],
+        end: endDate.toISOString().split('T')[0]
     };
-};
+}
 
-// Fonction pour traiter une salle
-const traiterSalle = async (nomSalle) => {
-    if (!nomSalle) return null;
+// Processes a room to add it to the database if it not already exists
+async function processRoom(roomName) {
+    // Checking if the room name is valid
+    if (!roomName) return null;
 
-    const nomFormate = nomSalle.includes("(")
-        ? nomSalle.split("(")[0].trim()
-        : nomSalle.trim();
-    const batiment = nomSalle.includes("(")
-        ? nomSalle.split("(")[1].split(")")[0]
-        : nomSalle;
+    // Formatting the room and building names
+    const formattedRoom = roomName.includes('(')
+        ? roomName.split('(')[0].trim()
+        : roomName.trim();
+    const formattedBuilding = roomName.includes('(')
+        ? roomName.split('(')[1].split(')')[0]
+        : roomName;
 
-    let salle = await Salle.findOne({ nom_salle: nomFormate });
+    // Trying to find the room in the database
+    let room = await Salle.findOne({ nom_salle: formattedRoom });
 
-    if (!salle) {
-        salle = new Salle({
-            nom_salle: nomFormate,
-            batiment: batiment,
+    // if the room does not exist, create a new record
+    if (!room) {
+        room = new Salle({
+            nom_salle: formattedRoom,
+            batiment: formattedBuilding,
             places_assises: 0,
         });
-        await salle.save();
-        console.log(
-            `\r\x1b[KNouvelle salle ajoutée : ${nomFormate} (${batiment})`
-        );
+        await room.save();
+        console.log(`\r\x1b[KNouvelle salle ajoutée : ${formattedRoom} (${formattedBuilding})`);
     }
-    return salle;
-};
 
-// Fonction pour traiter un cours individuel
-const traiterCours = async (donneesCours) => {
-    const salles = donneesCours.rooms_for_blocks.split(";");
-    const sallePrincipale = await traiterSalle(salles[0]);
+    return room;
+}
 
-    if (
-        !donneesCours.start_at ||
-        !donneesCours.end_at ||
-        !donneesCours.rooms_for_blocks ||
-        !sallePrincipale._id
-    ) {
+// Processes a course to add it to the database if it not already exists
+async function processCourse(courseData) {
+    // Getting rooms and then the main room
+    const rooms = courseData.rooms_for_blocks.split(';');
+    const mainRoom = await processRoom(rooms[0]);
+
+    // Checking if data is valid
+    if (!courseData.start_at || !courseData.end_at || !courseData.rooms_for_blocks || !mainRoom._id) {
         return;
     }
 
-    const coursExiste = await Cours.exists({
-        classe: sallePrincipale?._id,
-        debute_a: donneesCours.start_at,
-        fini_a: donneesCours.end_at,
+    // Checking if the course already exists in the database
+    const courseExists = await Cours.exists({
+        classe: mainRoom?._id,
+        debute_a: courseData.start_at,
+        fini_a: courseData.end_at,
     });
-    if (coursExiste) return;
+    if (courseExists) return;
 
-    const nouveauCours = new Cours({
-        identifiant: donneesCours.id,
-        debute_a: donneesCours.start_at,
-        fini_a: donneesCours.end_at,
-        professeur: donneesCours.teachers_for_blocks || "Non renseigné",
-        classe: sallePrincipale?._id || "Non renseigné",
-        module: donneesCours.modules_for_blocks || "Non renseigné",
-        groupe:
-            donneesCours.educational_groups_for_blocks
-                .split(";")
-                .map((item) => item.trim()) || "Non renseigné",
-        couleur: closestPaletteColor(donneesCours.color) || "#FF7675",
+    // If the course doesn't exists, create it the database
+    const newCourse = new Cours({
+        identifiant: courseData.id,
+        debute_a: courseData.start_at,
+        fini_a: courseData.end_at,
+        professeur: courseData.teachers_for_blocks || 'Non renseigné',
+        classe: mainRoom?._id || 'Non renseigné',
+        module: courseData.modules_for_blocks || 'Non renseigné',
+        groupe: courseData.educational_groups_for_blocks.split(';').map((item) => item.trim()) || 'Non renseigné',
+        couleur: closestPaletteColor(courseData.color) || '#FF7675',
     });
 
-    await nouveauCours.save();
-};
+    await newCourse.save();
+}
 
-// Fonction pour récupérer les cours d'un groupe
-const recupererCours = async (groupe) => {
-    const dates = obtenirDatesRequete();
-    process.stdout.write(
-        `Récupération des cours pour le groupe ${groupe.nom} du ${dates.debut} au ${dates.fin}\r`
-    );
-    const urlRequete = `https://edt-v2.univ-nantes.fr/events?start=${dates.debut}&end=${dates.fin}&timetables%5B%5D=${groupe.identifiant}`;
+// Retrieves courses for a group
+async function fetchCourses(group) {
+    // Getting dates for the specified amount of time
+    const dates = getRequestDates(DAYS_TO_RETRIEVE);
+
+    process.stdout.write(`\r\x1b[KRécupération des cours pour le groupe ${group.nom} du ${dates.start} au ${dates.end}`);
+
+    // Building request URL
+    const requestUrl = `https://edt-v2.univ-nantes.fr/events?start=${dates.start}&end=${dates.end}&timetables%5B%5D=${group.identifiant}`;
 
     try {
-        const reponse = await fetch(urlRequete);
-        const donnees = await reponse.json();
+        // Getting data from the Nantes Université timetable API
+        const response = await fetch(requestUrl);
+        const jsonData = await response.json();
 
-        for (const cours of donnees) {
-            await traiterCours(cours);
+        // Processing each course
+        for (const course of jsonData) {
+            await processCourse(course);
         }
 
-        io.emit("groupUpdated", { message: `Groupe ${groupe.nom} mis à jour` });
-    } catch (erreur) {
-        console.error(`Erreur pour le groupe ${groupe.nom} (id : ${groupe.identifiant}, url : ${urlRequete}) :`, erreur);
+        // Sending an update message to all clients
+        io.emit('groupUpdated', { message: `Groupe ${group.nom} mis à jour` });
+    } catch (error) {
+        console.error(`Erreur pour le groupe ${group.nom} (id : ${group.identifiant}, url : ${requestUrl}) :`, error);
     }
-};
+}
 
-// Fonction pour traiter un lot de groupes
-const traiterLotGroupes = async (groupes) => {
-    for (const groupe of groupes) {
-        await recupererCours(groupe);
+// Processes a batch of groups
+async function processBatchGroups(groups) {
+    for (const group of groups) {
+        await fetchCourses(group);
     }
-};
+}
 
-// Fonction principale d'exécution
-export const getCourses = async () => {
-    const groupes = await Groupe.find();
+// Main
+async function getCourses() {
+    const groups = await Groupe.find();
 
-    // Si FORCE est activé, traiter tous les groupes immédiatement
-    if (process.env.FORCER_TRAITEMENT_GPES === "true") {
-        console.log(
-            "Traitement de tous les groupes ACTIVÉ - Démarrage du processus..."
-        );
-        await traiterLotGroupes(groupes);
+    // If 'FORCER_TRAITEMENT_GPES' is activated, process all groups immediately
+    if (process.env.FORCER_TRAITEMENT_GPES === 'true') {
+        console.log('Traitement de tous les groupes ACTIVÉ - Démarrage du processus...');
+        await processBatchGroups(groups);
     } else {
-        console.log("Traitement de tous les groupes DÉSACTIVÉ");
+        console.log('Traitement de tous les groupes DÉSACTIVÉ');
     }
 
-    // Calculer l'intervalle entre chaque groupe pour une répartition sur 12h
-    const nombreGroupes = groupes.length;
-    const intervalleEntreGroupes = Math.floor(INTERVALLE_CYCLE / nombreGroupes);
+    // Calculating the interval between each group for a CYCLE_INTERVAL-hour distribution
+    const groupsNumber = groups.length;
+    const intervalBetweenGroups = Math.floor(CYCLE_INTERVAL / groupsNumber);
 
-    // Fonction pour démarrer le cycle de mise à jour
-    const demarrerCycleMiseAJour = () => {
-        let indexGroupe = 0;
+    // Function to start the update cycle
+    const startUpdateCycle = () => {
+        let groupIndex = 0;
 
-        const programmerProchainGroupe = () => {
-            if (indexGroupe < nombreGroupes) {
+        // Function to schedule the next group to update
+        const scheduleNextGroup = () => {
+            if (groupIndex < groupsNumber) {
                 setTimeout(async () => {
-                    const groupe = groupes[indexGroupe];
-
-                    await recupererCours(groupe);
-                    indexGroupe++;
-                    programmerProchainGroupe();
-                }, intervalleEntreGroupes);
-            } else {
-                // Réinitialiser pour le prochain cycle
-                indexGroupe = 0;
+                    const group = groups[groupIndex];
+                    await getCourses(group);
+                    groupIndex++;
+                    scheduleNextGroup();
+                }, intervalBetweenGroups);
+            } else { // all groups were processed
+                // Resetting the group index for the next cycle
+                groupIndex = 0;
                 setTimeout(() => {
-                    console.log("Démarrage d'un nouveau cycle de 12h");
-                    programmerProchainGroupe();
-                }, intervalleEntreGroupes);
+                    console.log('Démarrage d\'un nouveau cycle de 12h');
+                    scheduleNextGroup();
+                }, intervalBetweenGroups);
             }
         };
 
-        // Démarrer le premier cycle
-        programmerProchainGroupe();
+        // Starting to update the first group 
+        scheduleNextGroup();
     };
 
-    // Démarrer le cycle de mise à jour
-    console.log(
-        `Démarrage du cycle - ${nombreGroupes} groupes seront traités toutes les ${INTERVALLE_CYCLE / 1000 / 60 / 60}h`
-    );
-    console.log(
-        `Intervalle entre chaque groupe: ${intervalleEntreGroupes / 1000} secondes`
-    );
-    demarrerCycleMiseAJour();
-};
+    // Starting the update process
+    console.log('Démarrage du cycle de mise à jour...');
+    console.log(`${groupsNumber} groupes seront traités toutes les ${CYCLE_INTERVAL / 1000 / 60 / 60}h`);
+    console.log(`Intervalle entre chaque groupe : ${intervalBetweenGroups / 1000} secondes`);
+    startUpdateCycle();
+}
+
+export default getCourses;

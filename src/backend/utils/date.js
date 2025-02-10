43a0374d35dbf22118424786a33f086b8469cb18
devmlb <97614168+devmlb@users.noreply.github.com>
2025-02-10 16:03:22 +0100

    Fixed incorrect week number after loading a timetable

--- src/backend/utils/date.js
+++ src/backend/utils/date.js
@@ -42,20 +42,22 @@ function getWeekInfos(weekNumber) {
 
 // Returns the number of the current week
 function getWeeksNumber() {
-    const currentDate = new Date();
+    const currentDate = new Date(); // current date
     const startDate = new Date(currentDate.getFullYear(), 0, 1); // start date of the year (1st January)
+    const currentDay = currentDate.getDay(); // current day (0 for Sunday, ... , 6 for Saturday)
 
-    // Calculating the difference between the current date and the start of the year
-    const datesDiff = currentDate - startDate;
-    const daysDiff = datesDiff / (1000 * 60 * 60 * 24);
+    // Algorithm from https://perso.univ-lemans.fr/~hainry/articles/semaine.html
+    const J = startDate.getDay();
+    const N = Math.round((currentDate - startDate) / 1000 / 24 / 60 / 60);
 
-    // Calculating the number of weeks
-    let weekNumber = Math.ceil(daysDiff / 7);
+    let weekNumber;
+    if (J <= 4) {
+        weekNumber = Math.floor((J + N + 5) / 7);
+    } else {
+        weekNumber = Math.floor((J + N + 5) / 6);
+    }
 
-    // Getting the current day (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
-    const currentDay = currentDate.getDay();
-
-    // If it's Saturday (6) or Sunday (0), incrementing the week number
+    // If it's Saturday (6) or Sunday (0), incrementing the week number (weekend not displayed in the UI)
     if (currentDay === 6 || currentDay === 0) {
         weekNumber += 1;
     }

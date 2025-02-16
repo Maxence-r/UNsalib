6c005cd5d7426fc5ca2969ab276f6e333ab3ad74
devmlb <97614168+devmlb@users.noreply.github.com>
2025-02-16 11:15:31 +0100

    Added onClick prop to the button component

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,11 +1,12 @@
-import { ScriptProps } from "next/script";
 import "./button.css";
 import { ReactNode } from "react";
 
-export default function Button({ className, children }: { className: string, children: ReactNode }) {
+export default function Button({ className, children, onClick }: { className: string, children: ReactNode, onClick: React.MouseEventHandler<HTMLButtonElement> }) {
     return (
-        <button className={`button ${className}`}>
+        <button className={`button ${className}`} onClick={onClick}>
             <span className="button__text">{ children }</span>
         </button>
     );
 }
+
+Button.defaultProps = { className: "", onClick: (() => {}) };

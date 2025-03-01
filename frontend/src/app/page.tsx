1d241ce2efba3cfed7361be84614abbb3272896b
devmlb <97614168+devmlb@users.noreply.github.com>
2025-03-01 11:38:22 +0100

    Moved the rooms list component to the panel file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,8 @@ import App from './app';
 export default async function PreFetchData() {
     const response = await fetch(`${process.env.API_URI}/rooms`);
     const rooms: RoomsListType[] = await response.json();
+    // TODO: handle error when fetching
+    // const rooms = [];
 
     return <App prefetchedRoomsList={rooms}></App>;
 }

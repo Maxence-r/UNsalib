314b6592132a98bdf9c7919320bd5c4728b79ba1
devmlb <97614168+devmlb@users.noreply.github.com>
2025-03-02 09:03:19 +0100

    Bug fixes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,26 +5,25 @@ import "./globals.css";
 export const metadata: Metadata = {
     title: "UNsalib - Sciences et techniques",
     description: "Trouvez les salles libres de Nantes Université et affichez leurs emplois du temps",
-    themeColor: "#ffffff",
 };
 
 export const viewport: Viewport = {
     width: "device-width",
     initialScale: 1,
-    maximumScale: 1
+    maximumScale: 1,
+    themeColor: [
+        { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+        // { media: '(prefers-color-scheme: dark)', color: 'black' },
+    ]
 };
 
 const font = Space_Grotesk({
     weight: ['300', '400', '500', '600', '700'],
     subsets: ['latin'],
-    display: 'swap',
+    display: 'swap'
 });
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="fr" className={font.className}>
             <body>

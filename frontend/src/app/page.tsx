aa4c415ffae263f841ccb6fc25c14a842fa8819e
Mlb <97614168+devmlb@users.noreply.github.com>
2025-03-13 13:43:28 +0100

    Added code to handle rooms list prefetch errors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,13 @@ import { ApiRoomType } from "./types";
 import App from './app';
 
 export default async function PreFetchData() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/rooms`);
-    const rooms: ApiRoomType[] = await response.json();
-    // TODO: handle error when fetching
-    // const rooms = [];
+    let rooms: ApiRoomType[] = [];
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/rooms`);
+        rooms = await response.json();
+    } catch {
+        console.error("Impossible de récupérer la liste des salles");
+    }
 
     return <App prefetchedRoomsList={rooms}></App>;
 }

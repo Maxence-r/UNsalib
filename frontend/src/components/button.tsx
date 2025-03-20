d58fe3493a40ab6d3a435b17aaa2520bc9d1c173
Mlb <97614168+devmlb@users.noreply.github.com>
2025-03-20 16:42:31 +0100

    Added icon support to the button component

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,13 +1,15 @@
 import "./button.css";
 import "../theme.css";
 import { ReactNode } from "react";
+import Image from "next/image";
 
-export default function Button({ className, children, onClick }: { className: string, children: ReactNode, onClick: React.MouseEventHandler<HTMLButtonElement> }) {
+export default function Button({ className, children, iconPath, onClick }: { className: string, children: ReactNode, iconPath: string, onClick: React.MouseEventHandler<HTMLButtonElement> }) {
     return (
         <button className={`button ${className}`} onClick={onClick}>
+            {iconPath ? <Image className="button__icon" src={iconPath} alt="" height={24} width={24}></Image> : <></>}
             <span className="button__text">{children}</span>
         </button>
     );
 }
 
-Button.defaultProps = { className: "", onClick: (() => { }) };
+Button.defaultProps = { className: "", iconPath: "", onClick: (() => { }) };

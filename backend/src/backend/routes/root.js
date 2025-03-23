29ff7ee38b67e3a2d2da5b33652368bddbed245e
devmlb <97614168+devmlb@users.noreply.github.com>
2025-03-23 12:34:56 +0100

    Started to delete old stuff

--- backend/src/backend/routes/root.js
+++ backend/src/backend/routes/root.js
@@ -3,11 +3,7 @@ const router = Router();
 import 'dotenv/config';
 
 router.get('/', (req, res) => {
-    if (process.env.MAINTENANCE === 'true') {
-        res.sendFile('src/client/html/maintenance.html', { root: '.' });
-        return;
-    }
-    res.sendFile('src/client/html/main.html', { root: '.' });
+    res.sendFile('src/client/html/maintenance.html', { root: '.' });
 });
 
 

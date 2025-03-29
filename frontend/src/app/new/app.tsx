4e042a1ed1739aefd8bedb9b71af1a399d416b13
devmlb <97614168+devmlb@users.noreply.github.com>
2025-03-29 21:49:14 +0100

    Improved the onboarding page

--- frontend/src/app/new/app.tsx
+++ frontend/src/app/new/app.tsx
@@ -13,14 +13,16 @@ export default function App() {
                 />
             </div>
             <div className="content-container">
-                <h1 className="title">UNsalib fait peau neuve&nbsp;!</h1>
-                <h3 className="subtitle">Bienvenue sur la version 2.0</h3>
+                <div className="header">
+                    <h1 className="title">UNsalib fait peau neuve&nbsp;!</h1>
+                    <h3 className="subtitle">Bienvenue sur la version 2.0</h3>
+                </div>
                 <div className="features-card">
                     <ul className="features-list">
-                        <li className="feature"><i>🌙</i>Mode sombre automatique pour protéger vos yeux</li>
-                        <li className="feature"><i>📱</i>Navigation sur mobile comme dans une vrai app avec la prise en charge du bouton de retour</li>
-                        <li className="feature"><i>🦾</i>Rapidité et fiabilité accrues grâce à l&apos;utilisation de React</li>
-                        <li className="feature"><i>🖥️</i>L&apos;interface que vous connaissez</li>
+                        <li className="feature"><i>🌙</i>Mode sombre automatique pour protéger les yeux</li>
+                        <li className="feature"><i>📱</i>Navigation sur mobile comme dans une vrai app, avec la prise en charge du bouton de retour</li>
+                        <li className="feature"><i>🦾</i>L&apos;interface que vous connaissez, plus rapide et plus fiable grâce à l&apos;utilisation de React</li>
+                        <li className="feature"><i>⚡</i>Améliorations du temps de chargement des emplois du temps côté serveur</li>
                     </ul>
                     <Button onClick={() => window.location.href = "/"}>C&apos;est parti !</Button>
                 </div>

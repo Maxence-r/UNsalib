21e1fc2b581430d74f7bc1496bd23fdb6dfa73b0
Mlb <97614168+devmlb@users.noreply.github.com>
2025-04-04 13:30:48 +0200

    Removed unnecessary routes from the backend

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,20 @@
-import express, { json, urlencoded, static as serveStatic } from "express";
+import express, { json, urlencoded, static as serveStatic } from 'express';
 const app = express();
 import cors from 'cors';
 
-import { set, connect } from "mongoose";
-import "dotenv/config";
-import cookieParser from "cookie-parser";
-import launch from "./src/backend/background/main.js";
-import sallesApi from "./src/backend/routes/api/rooms.js";
-import adminApi from "./src/backend/routes/api/admin.js";
-import appInfosApi from "./src/backend/routes/api/app.js";
-import adminDashboard from "./src/backend/routes/admin.js";
-import authentication from "./src/backend/middlewares/auth.js";
-import stats from "./src/backend/middlewares/stats.js";
-import root from "./src/backend/routes/root.js";
+import { set, connect } from 'mongoose';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import launch from './src/backend/background/main.js';
+import sallesApi from './src/backend/routes/api/rooms.js';
+import adminApi from './src/backend/routes/api/admin.js';
+import appInfosApi from './src/backend/routes/api/app.js';
+import adminDashboard from './src/backend/routes/admin.js';
+import authentication from './src/backend/middlewares/auth.js';
+import stats from './src/backend/middlewares/stats.js';
 
 // SECURITE SERVER
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 app.use(cookieParser());
 
 // DEFAULT MIDDLEWARES
@@ -25,7 +24,7 @@ app.use(cors({
 }));
 app.use(json());
 app.use(urlencoded({ extended: true }));
-app.use(serveStatic("./src/client"));
+app.use(serveStatic('./src/client'));
 
 // Serve the .well-known directory
 app.use('/.well-known', serveStatic('./.well-known'));
@@ -33,26 +32,30 @@ app.use('/.well-known', serveStatic('./.well-known'));
 // API ROUTES
 app.use(authentication);
 app.use(stats);
-app.use("/api/rooms", sallesApi);
-app.use("/api/admin", adminApi);
-app.use("/api/app", appInfosApi);
+app.use('/api/rooms', sallesApi);
+app.use('/api/admin', adminApi);
+app.use('/api/app', appInfosApi);
 
 // OTHER ROUTES
-app.use("/admin", adminDashboard);
-app.use("/", root);
+app.use('/admin', adminDashboard);
+// 404 FALLBACK
+app.use((req, res) => {
+    res.status(404);
+    res.json({ error: 'NOT_FOUND' });
+});
 
 // DATABASE CONNECTION
-set("strictQuery", true);
+set('strictQuery', true);
 (async () => {
     try {
         await connect(`${process.env.MONGODB_URI}`, {});
-        console.log("Connexion à MongoDB réussie !");
+        console.log('Connexion à MongoDB réussie !');
     } catch (err) {
-        console.log("Connexion à MongoDB échouée !");
+        console.log('Connexion à MongoDB échouée !');
         console.log(err);
         process.exit(0);
     } finally {
-        // console.log('Jean-Michel', await hash("lesupermotdepasse", 10))
+        // console.log('Jean-Michel', await hash('lesupermotdepasse', 10))
         // EXECUTION PERMANENTE
         launch();
     }

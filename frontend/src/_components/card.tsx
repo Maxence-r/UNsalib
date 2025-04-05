4f7f6113171b6ccd940c97fd12b4b9de1a6337d3
devmlb <97614168+devmlb@users.noreply.github.com>
2025-04-05 15:35:14 +0200

    Added a card component

---
+++ frontend/src/_components/card.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from "react";
+
+import "@/_utils/theme.css";
+import "./card.css";
+
+export function CardHeader({ children }: { children: string }) {
+    return (
+        <div className="card-header">
+            <h3>{children}</h3>
+        </div>
+    )
+}
+
+export function CardContent({ children }: { children: ReactNode }) {
+    return (
+        <div className="card-content">
+            {children}
+        </div>
+    )
+}
+
+export function CardActions({ children }: { children: ReactNode }) {
+    return (
+        <div className="card-actions">
+            {children}
+        </div>
+    )
+}
+
+export function Card({
+    className,
+    children,
+    id,
+    highlighted,
+    elevated
+}: {
+    className: string,
+    children: ReactNode,
+    id: string,
+    highlighted: boolean,
+    elevated: boolean
+}) {
+    return (
+        <div className={`card${highlighted ? " highlighted" : ""}${elevated ? " elevated" : ""} ${className}`} id={id}>
+            {children}
+        </div>
+    );
+}
+
+Card.defaultProps = { className: "", id: "", highlighted: false, elevated: false };

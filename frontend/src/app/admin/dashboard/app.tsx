d56fc32fdaf6da15e5667f083949a28a99fc4c24
devmlb <97614168+devmlb@users.noreply.github.com>
2025-04-05 19:31:43 +0200

    Added a static dashboard page

---
+++ frontend/src/app/admin/dashboard/app.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import Image from "next/image";
+
+import Button from "@/_components/button";
+import { Card, CardContent, CardHeader, CardActions } from "@/_components/card";
+import { SwitchView } from "@/_components/switch";
+import { PieChart } from "@/_components/chart";
+
+export default function App() {
+    return (
+        <div className="page">
+            <div className="navbar">
+                <Image className="logo" src="/logo96.png" alt="logo" width={96} height={96}></Image>
+                <h1>UNsalib</h1>
+                <span className="spacer"></span>
+                <Button onClick={() => window.location.href = "/"}>Accueil</Button>
+            </div>
+            <div className="main dashboard">
+                <div className="main-content">
+                    <div className="section">
+                        <h4 className="section-title">Actions rapides</h4>
+                        <div className="section-content">
+                            <Card>
+                                <CardHeader>Mode maintenance</CardHeader>
+                                <CardContent>
+                                    <SwitchView title="Activé" onCheck={() => console.log("enabled")} onUncheck={() => console.log("disabled")}></SwitchView>
+                                </CardContent>
+                            </Card>
+                            <Card highlighted>
+                                <CardHeader>Nouvelles salles</CardHeader>
+                                <CardContent>
+                                    Aucune nouvelle salle.
+                                </CardContent>
+                                <CardActions><Button>Test</Button></CardActions>
+                            </Card>
+                            <Card>
+                                <CardHeader>Santé</CardHeader>
+                                <CardContent>
+                                    OK
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <h4 className="section-title">Statistiques globales</h4>
+                        <div className="section-content">
+                            <div className="column stats-overview">
+                                <Card>
+                                    <CardHeader>Visiteurs uniques</CardHeader>
+                                    <CardContent>
+                                        <h1>50</h1>
+                                    </CardContent>
+                                </Card>
+                                <Card>
+                                    <CardHeader>Vues</CardHeader>
+                                    <CardContent>
+                                        <h1>100</h1>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                            <div className="column">
+                                <Card>
+                                    <CardHeader>Plateformes</CardHeader>
+                                    <CardContent>
+                                        <PieChart 
+                                        dataset={
+                                            [
+                                                {legend: "Toto", value: 50},
+                                                {legend: "Tata", value: 100},
+                                                {legend: "Tutu", value: 200}
+                                            ]
+                                        }
+                                        sortDataset="asc"
+                                        chartId="platforms"
+                                        ></PieChart>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import "./dashboard.css";
+import App from "./app";
+import "@/_utils/theme.css";
+
+export default function Dashboard() {
+    return <App></App>
+}

511bd22ae51d7f270c49651f882c32acf8180a6e
devmlb <97614168+devmlb@users.noreply.github.com>
2025-04-06 17:24:13 +0200

    Created a new sidebar component for large screens

---
+++ frontend/src/app/admin/dashboard/_components/sidebar/sidebar.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useEffect, useRef, JSX } from "react";
+
+import Button from "@/_components/button";
+import { ApiUserAccount } from "@/app/admin/dashboard/_utils/types";
+import "./sidebar.css";
+
+type Action = {
+    onClick: () => void,
+    name: string,
+    iconType: "src" | "icon" | "base64" | "none",
+    icon: string
+};
+
+function Action({ onClick, name, iconType, icon }: Action) {
+    let iconNode = <></>;
+    if (iconType === "src") {
+        iconNode = <Image fill src={icon} alt="" />;
+    } else if (iconType === "base64") {
+        iconNode = <Image fill src={`data:image/png;base64,${icon}`} alt="" />;
+    } else if (iconType === "icon") {
+        iconNode = <i className="material-symbols-rounded">{icon}</i>;
+    }
+    return (
+        <button className="action" onClick={onClick} key={`action ${name}`}>
+            {iconNode}
+            <span>{name}</span>
+        </button>
+    );
+}
+
+function ActionsList({ actions }: { actions: Action[] }) {
+    return (
+        <div className="actions-list">
+            {actions.map(action => {
+                return (
+                    <Action onClick={action.onClick} name={action.name} iconType={action.iconType} icon={action.icon} key={`action ${action.name}`} />
+                );
+            })}
+        </div>
+    );
+}
+
+function useOutsideClickHandler(ref: React.RefObject<HTMLElement | null>, stateHook: boolean, setStateHook: (state: boolean) => void) {
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node) && stateHook) {
+                setStateHook(false);
+            }
+        };
+
+        document.addEventListener("click", handleClickOutside);
+
+        return () => document.removeEventListener("click", handleClickOutside);
+    }, [ref, stateHook, setStateHook]);
+}
+
+export default function Sidebar({ userAccount, className }: { userAccount: ApiUserAccount, className: string }) {
+    const [openUserMenu, setOpenUserMenu] = useState<boolean>(false);
+    const accountMenuRef = useRef<HTMLDivElement | null>(null);
+    useOutsideClickHandler(accountMenuRef, openUserMenu, setOpenUserMenu);
+
+    const logout = async () => {
+        try {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/admin/auth/logout`,
+                { credentials: "include" }
+            );
+            const logoutState = await response.json();
+            if (logoutState.message === "LOGOUT_SUCCESSFUL") {
+                window.location.href = "/admin/auth";
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className={`sidebar ${className}`}>
+            <div className="branding">
+                <Image className="logo" src="/logo96.png" alt="logo" width={96} height={96}></Image>
+                <h1>UNsalib</h1>
+            </div>
+            <span className="divider"></span>
+            <ActionsList
+                actions={[
+                    { icon: "home", iconType: "icon", name: "Accueil", onClick: () => { } },
+                    { icon: "edit", iconType: "icon", name: "Gestion", onClick: () => { } },
+                    { icon: "query_stats", iconType: "icon", name: "Statistiques", onClick: () => { } }
+                ]}
+            />
+            <span className="spacer" />
+            <ActionsList
+                actions={[
+                    { icon: userAccount.icon, iconType: "base64", name: `${userAccount.name} ${userAccount.lastname}`, onClick: () => { } },
+                    { icon: "logout", iconType: "icon", name: "Déconnexion", onClick: () => { } }
+                ]}
+            />
+        </div >
+    );
+}

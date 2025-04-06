d0b3128539f493470fd919e0a332065f1416d660
devmlb <97614168+devmlb@users.noreply.github.com>
2025-04-06 12:03:25 +0200

    Moved the auth page form to a new file

---
+++ frontend/src/app/admin/beta/auth/form.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Button from "@/_components/button";
+import Input from "@/_components/input";
+
+export default function Form() {
+    const [displayError, setDisplayError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/admin/auth/login`,
+                {
+                    method: "POST",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        password: password,
+                    })
+                }
+            );
+            const loginState = await response.json();
+            if (loginState.error) {
+                if (loginState.error == "BAD_CREDENTIALS") {
+                    throw new Error("Nom d'utilisateur ou mot de passe incorrect");
+                } else if (loginState.error == 'INTERNAL_ERROR') {
+                    throw new Error("Erreur interne du serveur");
+                }
+                throw new Error();
+            } 
+            window.location.href = "/admin/beta/dashboard";
+        } catch (e) {
+            if (e instanceof Error) {
+                setDisplayError(e.toString().replace("Error: ", ""));
+            } else {
+                setDisplayError("Erreur inconnue");
+            }
+        }
+    }
+
+    return (
+        <>
+            {displayError != "" ? <div id="error-display">{displayError}</div> : <></>}
+            <Input id="username" type="text" placeholder="Entrez un nom d'utilisateur" value={username} onInput={(e) => setUsername((e.target as HTMLInputElement).value)} />
+            <Input id="password" type="password" placeholder="Entrez un mot de passe" value={password} onInput={(e) => setPassword((e.target as HTMLInputElement).value)} />
+            <Button id="submit-button" onClick={handleLogin}>Se connecter</Button>
+        </>
+    )
+}
--- frontend/src/app/admin/beta/auth/page.tsx
+++ frontend/src/app/admin/beta/auth/page.tsx
@@ -1,7 +1,5 @@
-"use client";
 import "./auth.css";
-import Button from "@/_components/button";
-import Input from "@/_components/input";
+import Form from "./form";
 
 export default function AuthPanel() {
     return (
@@ -11,10 +9,7 @@ export default function AuthPanel() {
                     <h1>UNsalib</h1>
                     <h2>Authentification</h2>
                 </div>
-                <div id="error-display"></div>
-                <Input id="username" type="text" placeholder="Entrez un nom d'utilisateur"></Input>
-                <Input id="password" type="password" placeholder="Entrez un mot de passe"></Input>
-                <Button id="submit-button" onClick={() => {}}>Se connecter</Button>
+                <Form />
             </div>
         </main>
     )

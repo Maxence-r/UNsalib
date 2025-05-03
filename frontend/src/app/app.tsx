d8893248a02b07c84b0a2245796a34d5ee598871
devmlb <97614168+devmlb@users.noreply.github.com>
2025-05-03 10:52:03 +0200

    Removed onboarding redirect

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -7,21 +7,9 @@ import Calendar from "./_components/calendar/calendar";
 import Modal from "@/_components/modal";
 import Toast from "@/_components/toast";
 import { ApiRoomsList } from "@/_utils/api-types";
-import { useVersionStore } from "./_utils/store";
 import NavigationManager from "@/_utils/navigation-manager";
 
 export default function App({ prefetchedRoomsList }: { prefetchedRoomsList: ApiRoomsList }) {
-    const isStorageHydrated = useVersionStore((state) => state.hasHydrated);
-    const v2Prompt = useVersionStore((state) => state.v2Prompt);
-    const setV2Prompt = useVersionStore((state) => state.setV2Prompt);
-
-    useEffect(() => {
-        if (isStorageHydrated && !v2Prompt) {
-            setV2Prompt();
-            window.location.href = "/new";
-        }
-    }, [isStorageHydrated]);
-
     return (
         <NavigationManager>
             <main tabIndex={-1} className="main">

a3d00a3c6124282e9f3716ea7a080e434be2e891
devmlb <97614168+devmlb@users.noreply.github.com>
2025-09-20 10:44:21 +0200

    Updated main

--- backend/src/background/main.ts
+++ backend/src/background/main.ts
@@ -1,37 +1,46 @@
-import "dotenv/config";
-
 import fixDb from "./fixDB.js";
 import getGroups from "./getGroups.js";
-import { getCourses, processBatchGroups, processGroup } from "./getCourses.js";
+import { getCourses, processBatchGroups, processGroup } from './getCourses.js';
+import 'dotenv/config'
 
-async function launch() {
-    // // If 'FORCER_RECUP_GPES' is activated, fetch all groups immediately
-    // console.log();
-    // if (process.env.FORCER_RECUP_GPES === 'true') {
-    //     console.log('Récupération des groupes ACTIVÉE - Démarrage du processus...');
-    //     await getGroups();
-    // } else {
-    //     console.log('Récupération des groupes DÉSACTIVÉE');
-    // }
-    // // If 'FORCER_TRAITEMENT_GPES' is activated, process all groups immediately
-    // console.log();
-    // if (process.env.FORCER_TRAITEMENT_GPES === 'true' || process.env.CORRIGER_GPES_INCORRECTS === 'true') {
-    //     console.log('Traitement de tous les groupes ACTIVÉ - Démarrage du processus...');
-    //     await processBatchGroups();
-    // } else {
-    //     console.log('Traitement de tous les groupes DÉSACTIVÉ');
-    // }
-    // // If 'FORCER_TRAITEMENT_GPES' is activated, process all groups immediately
-    // console.log();
-    // if (process.env.CORRIGER_GPES_INCORRECTS === 'true') {
-    //     console.log('Correction des groupes incorrects ACTIVÉE - Démarrage du processus...');
-    //     await fixDb();
-    // } else {
-    //     console.log('Correction des groupes incorrects DÉSACTIVÉE');
-    // }
-    // // await processGroup('L1')
-    // console.log();
-    // getCourses();
+async function launch(): Promise<void> {
+    // If 'FORCER_RECUP_GPES' is activated, fetch all groups immediately
+    console.log();
+    if (process.env.FORCER_RECUP_GPES === 'true') {
+        console.log('Récupération des groupes ACTIVÉE - Démarrage du processus...');
+        await getGroups();
+    } else {
+        console.log('Récupération des groupes DÉSACTIVÉE');
+    }
+
+    // If 'FORCER_TRAITEMENT_GPES' is activated, process all groups immediately
+    console.log();
+    if (process.env.FORCER_TRAITEMENT_GPES === 'true' || process.env.CORRIGER_GPES_INCORRECTS === 'true') {
+        console.log('Traitement de tous les groupes ACTIVÉ - Démarrage du processus...');
+        await processBatchGroups();
+    } else {
+        console.log('Traitement de tous les groupes DÉSACTIVÉ');
+    }
+    
+    // If 'FORCER_TRAITEMENT_GPES' is activated, process all groups immediately
+    console.log();
+    if (process.env.CORRIGER_GPES_INCORRECTS === 'true') {
+        console.log('Correction des groupes incorrects ACTIVÉE - Démarrage du processus...');
+        await fixDb();
+    } else {
+        console.log('Correction des groupes incorrects DÉSACTIVÉE');
+    }
+
+    // If 'SYNC_TIMETABLES' is activated, launch a loop executing the sync algorithm
+    console.log();
+    if (process.env.SYNC_TIMETABLES === 'true') {
+        console.log('Synchronisation des emplois du temps ACTIVÉE - Démarrage du processus...');
+        void getCourses();
+    } else {
+        console.log('Synchronisation des emplois du temps DÉSACTIVÉE');
+    }
+
+    // processGroup("387");
 }
 
 export default launch;

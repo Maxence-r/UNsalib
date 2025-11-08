9576a61cbd1efe3016c374d00004ff2d35b0ea5e
Mlb <97614168+devmlb@users.noreply.github.com>
2025-11-08 18:47:10 +0100

    Added a new chart to the stat page
    - Unique visitors VS unique human visitors during the current month

--- frontend/src/app/admin/dashboard/_components/stats/stats.tsx
+++ frontend/src/app/admin/dashboard/_components/stats/stats.tsx
@@ -3,11 +3,23 @@
 import { useEffect, useState } from "react";
 
 import { Card, CardContent, CardHeader } from "@/_components/card";
+import { BarChart } from "@/_components/chart";
 import "./stats.css";
-import { getDayUniqueHumanVisitors, getDayUniqueVisitors } from "../../_utils/client-actions";
+import { ApiUniqueHumanVisitors, ApiUniqueVisitors } from "@/_utils/api-types";
+import {
+    getDayUniqueHumanVisitors,
+    getDayUniqueVisitors,
+    getMonthUniqueHumanVisitors,
+    getMonthUniqueVisitors,
+} from "../../_utils/client-actions";
 
 export default function StatsPage() {
-    const [dayUniqueHumanVisitors, setDayUniqueHumanVisitors] = useState<string>("-");
+    const [dayUniqueHumanVisitors, setDayUniqueHumanVisitors] =
+        useState<string>("-");
+    const [monthUniqueHumanVisitors, setMonthUniqueHumanVisitors] =
+        useState<ApiUniqueHumanVisitors>({});
+    const [monthUniqueVisitors, setMonthUniqueVisitors] =
+        useState<ApiUniqueVisitors>({});
     const [dayUniqueVisitors, setDayUniqueVisitors] = useState<string>("-");
 
     useEffect(() => {
@@ -15,15 +27,23 @@ export default function StatsPage() {
         const fetchDayUniqueHumanVisitors = async () => {
             const raw = await getDayUniqueHumanVisitors();
             setDayUniqueHumanVisitors(raw.data[today].toString());
-        }
+        };
         const fetchDayUniqueVisitors = async () => {
             const raw = await getDayUniqueVisitors();
             setDayUniqueVisitors(raw.data[today].toString());
-        }
+        };
+        const fetchMonthUniqueHumanVisitors = async () => {
+            setMonthUniqueHumanVisitors(await getMonthUniqueHumanVisitors());
+        };
+        const fetchMonthUniqueVisitors = async () => {
+            setMonthUniqueVisitors(await getMonthUniqueVisitors());
+        };
 
         fetchDayUniqueHumanVisitors();
         fetchDayUniqueVisitors();
-    }, [setDayUniqueHumanVisitors]);
+        fetchMonthUniqueHumanVisitors();
+        fetchMonthUniqueVisitors();
+    }, []);
 
     return (
         <div className="main dashboard-stats">
@@ -34,7 +54,11 @@ export default function StatsPage() {
                         <h4 className="section-title">Aujourd&apos;hui</h4>
                         <div className="section-content">
                             <div className="column stats-overview">
-                                <Card isLoading={dayUniqueVisitors === "-" ? true : false}>
+                                <Card
+                                    isLoading={
+                                        dayUniqueVisitors === "-" ? true : false
+                                    }
+                                >
                                     <CardHeader>Visiteurs uniques</CardHeader>
                                     <CardContent>
                                         <h1>{dayUniqueVisitors}</h1>
@@ -42,10 +66,56 @@ export default function StatsPage() {
                                 </Card>
                             </div>
                             <div className="column stats-overview">
-                                <Card isLoading={dayUniqueHumanVisitors === "-" ? true : false}>
-                                    <CardHeader>Visiteurs humains uniques</CardHeader>
+                                <Card
+                                    isLoading={
+                                        dayUniqueHumanVisitors === "-"
+                                            ? true
+                                            : false
+                                    }
+                                >
+                                    <CardHeader>
+                                        Visiteurs humains uniques
+                                    </CardHeader>
                                     <CardContent>
                                         <h1>{dayUniqueHumanVisitors}</h1>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <h4 className="section-title">Ce mois-ci</h4>
+                        <div className="section-content">
+                            <div style={{ maxWidth: "100%" }}>
+                                <Card>
+                                    <CardHeader>Visites</CardHeader>
+                                    <CardContent>
+                                        <BarChart
+                                            style={{ height: 200 }}
+                                            dataset={Object.keys(
+                                                monthUniqueVisitors,
+                                            ).map((day) => ({
+                                                legend: day.substring(
+                                                    day.length - 2,
+                                                    day.length,
+                                                ),
+                                                y: [
+                                                    {
+                                                        value: monthUniqueVisitors[
+                                                            day
+                                                        ],
+                                                        group: "Visiteurs uniques",
+                                                    },
+                                                    {
+                                                        value: monthUniqueHumanVisitors[
+                                                            day
+                                                        ],
+                                                        group: "Visiteurs humains uniques",
+                                                    },
+                                                ],
+                                            }))}
+                                            chartId="platforms"
+                                        />
                                     </CardContent>
                                 </Card>
                             </div>

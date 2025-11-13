0b3b26c97040143b2e7c57c8a75ce600c4883060
Mlb <97614168+devmlb@users.noreply.github.com>
2025-11-13 14:32:07 +0100

    Improved maintenance page

--- frontend/src/app/maintenance/app.tsx
+++ frontend/src/app/maintenance/app.tsx
@@ -1,13 +1,39 @@
 import { Flame } from "lucide-react";
+import { redirect, RedirectType } from 'next/navigation';
 
 export default function App() {
-  return (
-    <div className="page">
-      <Flame className="icon" size={96} />
-      <div className="infos">
-        <h1>Maintenance</h1>
-        <div>UNsalib sera bientôt disponible.</div>
-      </div>
-    </div>
-  );
+    if (process.env.MAINTENANCE === "false") {
+        redirect("/", RedirectType.replace);
+    }
+
+    return (
+        <div className="page">
+            <Flame className="icon" size={96} />
+            <div
+                className="infos"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: 24,
+                    alignItems: "center",
+                    textAlign: "center",
+                }}
+            >
+                <h1>Maintenance</h1>
+                <div>
+                    Nous faisons notre maximum pour rétablir UNsalib au plus
+                    vite.
+                </div>
+                <div
+                    style={{
+                        fontSize: 14,
+                        marginTop: "30%",
+                        color: "var(--neutral-color-dark)",
+                    }}
+                >
+                    Vous pouvez prendre une petite pause en attendant ! 😉
+                </div>
+            </div>
+        </div>
+    );
 }

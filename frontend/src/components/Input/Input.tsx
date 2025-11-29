2303393124d4d951bde7021970a5e7943ba7705e
devmlb <97614168+devmlb@users.noreply.github.com>
2025-11-29 22:13:22 +0100

    Improved input component

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,42 +1,34 @@
 import "../../utils/theme.css";
 import "./Input.css";
 
-export default function Input({ 
-    className,
-    id,
-    type, 
-    placeholder,
-    value,
-    onInput,
-    onKeyDown
-}: { 
-    className: string,
-    id: string, 
-    type: string,
-    placeholder: string,
-    value: string,
-    onInput: React.KeyboardEventHandler<HTMLInputElement>,
-    onKeyDown: React.KeyboardEventHandler<HTMLInputElement>
+function Input({
+    className = "",
+    id = "",
+    type = "text",
+    placeholder = "",
+    value = "",
+    onInput = () => {},
+    onKeyDown = () => {},
+}: {
+    className?: string;
+    id?: string;
+    type?: string;
+    placeholder?: string;
+    value?: string;
+    onInput?: React.KeyboardEventHandler<HTMLInputElement>;
+    onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
 }) {
     return (
-        <input 
-        id={id} 
-        className={`input ${className}`} 
-        type={type} 
-        placeholder={placeholder} 
-        value={value} 
-        onInput={onInput}
-        onKeyDown={onKeyDown}
+        <input
+            id={id}
+            className={className ? `input ${className}` : "input"}
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onInput={onInput}
+            onKeyDown={onKeyDown}
         />
     );
 }
 
-Input.defaultProps = { 
-    className: "", 
-    id: "", 
-    type: "text", 
-    placeholder: "", 
-    value: "", 
-    onInput: (() => { }), 
-    onKeyDown: (() => { }) 
-};
+export { Input };

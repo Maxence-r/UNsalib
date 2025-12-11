31eab5a396344a0738bc29c454c11151e8a6d65c
Mlb <97614168+devmlb@users.noreply.github.com>
2025-12-11 08:55:33 +0100

    Cleaning

--- frontend/src/pages/app/calendar/calendar.tsx
+++ frontend/src/pages/app/calendar/calendar.tsx
@@ -4,12 +4,12 @@ import { ChevronUp } from "lucide-react";
 import { useSelectedRoomStore } from "../../../stores/app.store.js";
 import { TextButton } from "../../../components/button/Button.js";
 import "./calendar.css";
-import {
-    START_DAY_HOUR,
-    END_DAY_HOUR,
-    DAY_DURATION,
-    WEEK_DAYS,
-} from "../../../utils/constants.js";
+// import {
+//     START_DAY_HOUR,
+//     END_DAY_HOUR,
+//     DAY_DURATION,
+//     WEEK_DAYS,
+// } from "../../../utils/constants.js";
 // import CalendarContainer from "./grid/container.js";
 import { showToast, setToastMessage } from "../../../components/toast/Toast.js";
 import { goBack } from "../../../utils/navigation-manager.js";
@@ -23,34 +23,34 @@ import { ActionBar } from "./action-bar/ActionBar.js";
 import { Grid } from "./grid/Grid.js";
 
 function Calendar() {
-    function computeHourIndicator() {
-        const dateActuelle = new Date();
-        const jourActuel = dateActuelle.getDay();
-        const heureActuelle = dateActuelle.getHours();
-        const minuteActuelle = dateActuelle.getMinutes();
-        if (
-            heureActuelle >= START_DAY_HOUR &&
-            heureActuelle < END_DAY_HOUR &&
-            jourActuel > 0 &&
-            jourActuel <= WEEK_DAYS.length
-        ) {
-            const top =
-                (100 * (heureActuelle - START_DAY_HOUR)) / DAY_DURATION +
-                (100 / DAY_DURATION) * (minuteActuelle / 60);
-            return {
-                value:
-                    heureActuelle +
-                    ":" +
-                    (minuteActuelle.toString().length == 2
-                        ? minuteActuelle
-                        : "0" + minuteActuelle),
-                top: top.toString(),
-                display: true,
-            };
-        } else {
-            return { value: "", top: "", display: false };
-        }
-    }
+    // function computeHourIndicator() {
+    //     const dateActuelle = new Date();
+    //     const jourActuel = dateActuelle.getDay();
+    //     const heureActuelle = dateActuelle.getHours();
+    //     const minuteActuelle = dateActuelle.getMinutes();
+    //     if (
+    //         heureActuelle >= START_DAY_HOUR &&
+    //         heureActuelle < END_DAY_HOUR &&
+    //         jourActuel > 0 &&
+    //         jourActuel <= WEEK_DAYS.length
+    //     ) {
+    //         const top =
+    //             (100 * (heureActuelle - START_DAY_HOUR)) / DAY_DURATION +
+    //             (100 / DAY_DURATION) * (minuteActuelle / 60);
+    //         return {
+    //             value:
+    //                 heureActuelle +
+    //                 ":" +
+    //                 (minuteActuelle.toString().length == 2
+    //                     ? minuteActuelle
+    //                     : "0" + minuteActuelle),
+    //             top: top.toString(),
+    //             display: true,
+    //         };
+    //     } else {
+    //         return { value: "", top: "", display: false };
+    //     }
+    // }
 
     const [courses, setCourses] = useState<{
         courses: ApiCourse[];
@@ -67,24 +67,24 @@ function Calendar() {
     const [previousIncrement, setPreviousIncrement] = useState(0);
     const selectedRoom = useSelectedRoomStore((state) => state.room);
     const [isTimetableLoading, setTimetableLoadState] = useState(false);
-    const [hourIndicatorValue, setHourIndicatorValue] = useState(
-        computeHourIndicator().value,
-    );
-    const [hourIndicatorTop, setHourIndicatorTop] = useState(
-        computeHourIndicator().top,
-    );
-    const [displayHourIndicator, setHourIndicatorDisplay] = useState(false);
+    // const [hourIndicatorValue, setHourIndicatorValue] = useState(
+    //     computeHourIndicator().value,
+    // );
+    // const [hourIndicatorTop, setHourIndicatorTop] = useState(
+    //     computeHourIndicator().top,
+    // );
+    // const [displayHourIndicator, setHourIndicatorDisplay] = useState(false);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const hourIndicatorProperties = computeHourIndicator();
-            setHourIndicatorDisplay(hourIndicatorProperties.display);
-            setHourIndicatorValue(hourIndicatorProperties.value);
-            setHourIndicatorTop(hourIndicatorProperties.top);
-        }, 1000);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         const hourIndicatorProperties = computeHourIndicator();
+    //         setHourIndicatorDisplay(hourIndicatorProperties.display);
+    //         setHourIndicatorValue(hourIndicatorProperties.value);
+    //         setHourIndicatorTop(hourIndicatorProperties.top);
+    //     }, 1000);
 
-        return () => clearInterval(interval);
-    }, [hourIndicatorValue]);
+    //     return () => clearInterval(interval);
+    // }, [hourIndicatorValue]);
 
     useEffect(() => {
         async function render() {

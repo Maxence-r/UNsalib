b3498248d2beb209a48446e329820223da18219d
devmlb <97614168+devmlb@users.noreply.github.com>
2025-12-26 15:38:09 +0100

    Added a new font

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -6,6 +6,7 @@ import "@fontsource/space-grotesk/400.css";
 import "@fontsource/space-grotesk/500.css";
 import "@fontsource/space-grotesk/600.css";
 import "@fontsource/space-grotesk/700.css";
+import "@fontsource/cal-sans/400.css";
 
 import "../utils/theme.css";
 import "./main.css";

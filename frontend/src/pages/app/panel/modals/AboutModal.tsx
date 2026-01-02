a16daf96ba1b21db4bd991db55ffd613f17214c3
devmlb <97614168+devmlb@users.noreply.github.com>
2026-01-02 22:10:55 +0100

    New layout tests for the about modal

--- frontend/src/pages/app/panel/modals/AboutModal.tsx
+++ frontend/src/pages/app/panel/modals/AboutModal.tsx
@@ -1,180 +1,74 @@
 import { Users, BookOpen, Smile, Link2, ArrowUpRight } from "lucide-react";
 
 import { VERSION_NAME, VERSION_NUMBER } from "../../../../utils/constants.js";
-import { Modal } from "../../../../components/modal/Modal.js";
 import "./AboutModal.css";
 
-function AboutModal({
-    isOpen,
-    setIsOpen,
-}: {
-    isOpen: boolean;
-    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
+import {
+    Card,
+    CardHeader,
+    CardContent,
+} from "../../../../components/card/Card.js";
+
+function AboutModal() {
     return (
-        <Modal isOpen={isOpen} close={() => setIsOpen(false)}>
-            <div className="about">
-                <div className="modal-section">
-                    <h4 className="title">
-                        <BookOpen size={20} />À PROPOS
-                        <span
-                            id="version"
-                            onClick={() => window.open("/new", "_blank")}
-                        >
-                            v{VERSION_NUMBER} &quot;{VERSION_NAME}&quot;
-                        </span>
-                    </h4>
-                    <div className="content">
-                        <p>
-                            <strong>UNsalib</strong> est un site web open source
-                            qui permet aux étudiants et professeurs de Nantes
-                            Université de{" "}
-                            <strong>trouver les salles libres</strong> du campus
-                            et d&apos;afficher leurs{" "}
-                            <strong>emplois du temps</strong>.
-                        </p>
-                    </div>
-                </div>
-                <div className="modal-section">
-                    <h4 className="title">
-                        <Users size={20} />
-                        L&apos;ÉQUIPE
-                    </h4>
-                    <div className="content">
-                        <p>
-                            Nous sommes trois étudiants motivés pour améliorer
-                            le quotidien de tous au sein de l&apos;Université.
-                        </p>
-                        <div className="grid" id="team">
-                            <div
-                                className="item"
-                                onDoubleClick={() =>
-                                    (window.location.href = "/admin")
-                                }
-                            >
-                                <div className="infos">
-                                    <h2>Maxence</h2>
-                                    <p>Développeur & UX/UI designer</p>
-                                </div>
-                                <img
-                                    src="/maxence.png"
-                                    width={36}
-                                    height={36}
-                                    alt=""
-                                ></img>
-                            </div>
-                            <div
-                                className="item"
-                                onDoubleClick={() =>
-                                    (window.location.href = "/admin")
-                                }
-                            >
-                                <div className="infos">
-                                    <h2>Maël</h2>
-                                    <p>Développeur</p>
-                                </div>
-                                <img
-                                    src="/mael.png"
-                                    width={64}
-                                    height={64}
-                                    alt=""
-                                ></img>
-                            </div>
-                            <div
-                                className="item"
-                                onDoubleClick={() =>
-                                    (window.location.href = "/admin")
-                                }
-                            >
-                                <div className="infos">
-                                    <h2>Ethann</h2>
-                                    <p>Admin DB & testeur</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="modal-section">
-                    <h4 className="title">
-                        <Smile size={20} />
-                        REMERCIEMENTS
-                    </h4>
-                    <div className="content">
-                        <p>
-                            Merci à <strong>Christophe Lino</strong> pour sa
-                            confiance et son implication dans le projet, et par
-                            ailleurs pour avoir été le premier professeur à
-                            utiliser UNsalib.
-                        </p>
-                        <p>
-                            Merci aussi à{" "}
-                            <strong>tous ceux qui nous ont encouragés</strong>{" "}
-                            et qui nous soutiennent dans cette aventure.
-                        </p>
-                        <p>
-                            Merci enfin à <strong>Nantes Université</strong>{" "}
-                            d&apos;avoir publié les emplois du temps des
-                            différentes formations en libre accès.
-                        </p>
-                    </div>
-                </div>
-                <div className="modal-section">
-                    <h4 className="title">
-                        <Link2 size={20} />
-                        LIENS
-                    </h4>
-                    <div className="content">
-                        <div className="grid">
-                            <div className="item">
-                                <div className="infos">
-                                    <h2>White Paper</h2>
-                                    <p>Infos de conception</p>
-                                </div>
-                                <button
-                                    onClick={() =>
-                                        window.open(
-                                            "/white-paper.pdf",
-                                            "_blank",
-                                        )
-                                    }
-                                >
-                                    <ArrowUpRight size={20} />
-                                </button>
-                            </div>
-                            <div className="item">
-                                <div className="infos">
-                                    <h2>Github</h2>
-                                    <p>Code source</p>
-                                </div>
-                                <button
-                                    onClick={() =>
-                                        window.open(
-                                            "https://github.com/Maxence-r/UNsalib_Modern-Timetable-Viewer",
-                                            "_blank",
-                                        )
-                                    }
-                                >
-                                    <ArrowUpRight size={20} />
-                                </button>
-                            </div>
-                            <div className="item">
-                                <div className="infos">
-                                    <h2>Nouveautés</h2>
-                                    <p>Historique des versions</p>
-                                </div>
-                                <button
-                                    onClick={() =>
-                                        window.open("/new", "_blank")
-                                    }
-                                >
-                                    <ArrowUpRight size={20} />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className="about">
+            <div className="project">
+                <Card className="description">
+                    <CardHeader text="Vous cherchez une salle ?" />
+                    <CardContent>
+                        UNsalib permet aux étudiants et professeurs de profiter
+                        pleinement des infrastructures de Nantes Université.
+                        Travail en groupe, révisions, besoin de grands tableaux
+                        ou encore d’ordinateurs : trouvez les salles libres du
+                        campus en quelques instants et concentrez-vous sur
+                        l’essentiel !
+                    </CardContent>
+                </Card>
+                <Card className="credits">
+                    <CardHeader text="Remerciements" />
+                    <CardContent>
+                        Merci à tous ceux qui nous ont encouragés et soutenus,
+                        en particulier notre professeur, M. Christophe Lino.
+                        Merci également à Nantes Université de nous donner accès
+                        librement aux emplois du temps des différentes
+                        formations.
+                    </CardContent>
+                </Card>
+                <Card className="version">
+                    <CardHeader text='v3.0 "Louisa"' />
+                    <CardContent>Historique des versions</CardContent>
+                </Card>
+                <Card className="code">
+                    <CardHeader text="Code source" />
+                    <CardContent>Github</CardContent>
+                </Card>
             </div>
-        </Modal>
+            <div className="creators">
+                <h4 className="title">Qui sommes-nous ?</h4>
+                <Card className="ethann">
+                    <CardHeader text="Ethann" />
+                    <CardContent>Github</CardContent>
+                </Card>
+                <Card className="mael">
+                    <CardHeader text="Maël" />
+                    <CardContent>Github</CardContent>
+                </Card>
+                <Card className="maxence">
+                    <CardHeader text="Maxence" />
+                    <CardContent>Github</CardContent>
+                </Card>
+                <Card className="contact">
+                    <CardHeader text="Contact" />
+                    <CardContent>contact@unsalib.info</CardContent>
+                </Card>
+                <Card className="feedback">
+                    <CardHeader text="Avis" />
+                    <CardContent>
+                        Faites-nous part de vos suggestions, remarques...
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
     );
 }
 

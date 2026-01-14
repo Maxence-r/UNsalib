5a108b3fb5e71da7d11ca6f520d26ab460e09917
Mlb <97614168+devmlb@users.noreply.github.com>
2026-01-14 13:08:25 +0100

    Groups fetching structure modification

---
+++ backend/src/configs/data.config.ts
@@ -0,0 +1,11 @@
+const config = {
+    baseUrl: "https://edt-v2.univ-nantes.fr",
+    campus: [
+        {
+            name: "Sciences et techniques",
+            timetableIds: ["sciences"],
+        },
+    ],
+};
+
+export { config };
